020f0e73eb0082287e24b2345943cf2ffcf7e2ab
user <user@ROBOT.local>
2022-06-05 06:22:43 +0700

    done

--- index.js
+++ index.js
@@ -21,10 +21,32 @@ const dataBelanjaan = [
 ];
 
 // boleh dimodifikasi bila ingin menggunakan deklarasi fungsi yang normal
-const listBelanjaan = null;
+// const listBelanjaan = null;
+
+function listBelanjaan(dataBelanjaan) {
+  let list = [];
+  for (let i = 0; i < dataBelanjaan.length; i++) {
+    list.push(
+        "- " +
+        dataBelanjaan[i].nama +
+        " x " +
+        dataBelanjaan[i].kuantitas 
+    );
+  }
+  return list;
+}
+
 
 // boleh dimodifikasi bila ingin menggunakan deklarasi fungsi yang normal
-const totalBelanjaan = null;
+// const totalBelanjaan = null;
+
+function totalBelanjaan(dataBelanjaan) {
+  let total = 0;
+  for (let i = 0; i < dataBelanjaan.length; i++) {
+    total += dataBelanjaan[i].harga * dataBelanjaan[i].kuantitas;
+  }
+  return total;
+}
 
 // ! JANGAN DIMODIFIKASI
 const main = () => {
